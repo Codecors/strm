d03b9b8268811b67d9e9396fb5cf246a4191bd69
Codecors <info@stylehairs.tk>
2017-03-23 12:44:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,20 @@ app.set('view engine', 'hjs');
 //	Add cors to make jQuery API requests
 //
 app.use(cors());
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://myplay.ga/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
 //
 //	Check for HTTPS
 //
