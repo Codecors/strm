f57d74728133144b5e6deece545f11dc5a9bef32
Codecors <info@stylehairs.tk>
2017-03-24 13:19:48 +0530

    Update video.js

--- routes/video.js
+++ routes/video.js
@@ -2,6 +2,21 @@ let fs = require("fs")
 let path = require("path");
 let express = require('express');
 let WebTorrent = require('webtorrent')
+let app = express();
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://myplay.ga');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
 
 let router = express.Router();
 
